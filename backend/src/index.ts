e80104e40092a456cfd85616c21bae5d711b4c6a
Jason Zhang <jzhang5137@gmail.com>
2025-05-07 14:09:46 -0700

    feat(be): adds axios to fetch all digimon cards from digimoncard.io

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,30 @@
+/* eslint-disable no-console */
 import { serve } from "@hono/node-server";
+import axios from "axios";
 import { Hono } from "hono";
 
 const app = new Hono();
+
+const port = 3000;
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
+app.get("/allcards", async (c) => {
+  try {
+    const response = await axios.get("https://digimoncard.io/api-public/getAllCards.php?sort=name&series=Digimon%20Card%20Game&sortdirection=asc");
+    console.log(response);
+  }
+  catch (err) {
+    console.log("Error while getting all cards.");
+    console.log(err);
+  }
+});
+
+console.log(`Server is running on http://localhost:${port}`);
+
 serve({
   fetch: app.fetch,
-  port: 3000,
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`);
+  port,
 });
