d2e46409ab5138c1185734c6e99d09a1636fb6dd
Jason Zhang <jzhang5137@gmail.com>
2025-05-07 16:57:50 -0700

    simple cache

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,4 @@
-/* eslint-disable no-console */
 import { serve } from "@hono/node-server";
-import axios from "axios";
 import { Hono } from "hono";
 
 const app = new Hono();
@@ -11,17 +9,7 @@ app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
-app.get("/allcards", async (c) => {
-  try {
-    const response = await axios.get("https://digimoncard.io/api-public/getAllCards.php?sort=name&series=Digimon%20Card%20Game&sortdirection=asc");
-    console.log(response);
-  }
-  catch (err) {
-    console.log("Error while getting all cards.");
-    console.log(err);
-  }
-});
-
+// eslint-disable-next-line no-console
 console.log(`Server is running on http://localhost:${port}`);
 
 serve({
